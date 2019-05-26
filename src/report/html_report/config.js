911587238389e728262b8e5199e80e855af6a602
Oleg Semko <semoasem@gmail.com>
2019-05-26 09:52:07 +0300

    deploy static content

---
+++ src/report/html_report/config.js
@@ -0,0 +1,58 @@
+report({
+  "testSuite": "BackstopJS",
+  "tests": [
+    {
+      "pair": {
+        "reference": "../bitmaps_reference/test_test-card-default_0_data-qacard_0_tablet_h.png",
+        "test": "../bitmaps_test/20190526-075323/test_test-card-default_0_data-qacard_0_tablet_h.png",
+        "selector": "[data-qa=\"card\"]",
+        "fileName": "test_test-card-default_0_data-qacard_0_tablet_h.png",
+        "label": "test-card-default",
+        "requireSameDimensions": true,
+        "misMatchThreshold": 1,
+        "url": "http://localhost:8080/index.html",
+        "referenceUrl": "https://mate-academy.github.io/layout_solutions/product-cards/",
+        "expect": 0,
+        "viewportLabel": "tablet_h",
+        "diff": {
+          "isSameDimensions": false,
+          "dimensionDifference": {
+            "width": 0,
+            "height": -2
+          },
+          "misMatchPercentage": "15.96",
+          "analysisTime": 89
+        },
+        "diffImage": "../bitmaps_test/20190526-075323/failed_diff_test_test-card-default_0_data-qacard_0_tablet_h.png"
+      },
+      "status": "fail"
+    },
+    {
+      "pair": {
+        "reference": "../bitmaps_reference/test_test-card-hover_0_data-qacard_0_tablet_h.png",
+        "test": "../bitmaps_test/20190526-075323/test_test-card-hover_0_data-qacard_0_tablet_h.png",
+        "selector": "[data-qa=\"card\"]",
+        "fileName": "test_test-card-hover_0_data-qacard_0_tablet_h.png",
+        "label": "test-card-hover",
+        "requireSameDimensions": true,
+        "misMatchThreshold": 1,
+        "url": "http://localhost:8080/index.html",
+        "referenceUrl": "https://mate-academy.github.io/layout_solutions/product-cards/",
+        "expect": 0,
+        "viewportLabel": "tablet_h",
+        "diff": {
+          "isSameDimensions": false,
+          "dimensionDifference": {
+            "width": 0,
+            "height": -2
+          },
+          "misMatchPercentage": "15.96",
+          "analysisTime": 92
+        },
+        "diffImage": "../bitmaps_test/20190526-075323/failed_diff_test_test-card-hover_0_data-qacard_0_tablet_h.png"
+      },
+      "status": "fail"
+    }
+  ],
+  "id": "test"
+});
