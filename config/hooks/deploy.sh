c1eae6ec5f8e398a864b2d7899e45d3b8c94064c
«Jka4» <plotnik1295@gmail.com>
2019-05-26 15:47:57 +0300

    fin

--- config/hooks/deploy.sh
+++ config/hooks/deploy.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-cp -R backstop_data/ src/report
+cp -r backstop_data/ src/report
 git add src/
 git commit -m "deploy static content"
 git subtree push --prefix src origin gh-pages
