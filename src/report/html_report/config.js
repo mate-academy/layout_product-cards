4f510079c24160fd519658f76e205272b9b57792
Alina Kizilova <alinakzilova@gmail.com>
2019-07-28 11:27:05 +0300

    deploy static content

---
+++ src/report/html_report/config.js
@@ -0,0 +1,55 @@
+report({
+  "testSuite": "BackstopJS",
+  "tests": [
+    {
+      "pair": {
+        "reference": "..\\bitmaps_reference\\1222062857_test-card-default_0_data-qacard_0_tablet_h.png",
+        "test": "..\\bitmaps_test\\20190728-111558\\1222062857_test-card-default_0_data-qacard_0_tablet_h.png",
+        "selector": "[data-qa=\"card\"]",
+        "fileName": "1222062857_test-card-default_0_data-qacard_0_tablet_h.png",
+        "label": "test-card-default",
+        "requireSameDimensions": false,
+        "misMatchThreshold": 1,
+        "url": "http://localhost:8080/index.html",
+        "referenceUrl": "https://mate-academy.github.io/layout_solutions/product-cards/",
+        "expect": 0,
+        "viewportLabel": "tablet_h",
+        "diff": {
+          "isSameDimensions": false,
+          "dimensionDifference": {
+            "width": 0,
+            "height": 1
+          },
+          "misMatchPercentage": "0.32",
+          "analysisTime": 13
+        }
+      },
+      "status": "pass"
+    },
+    {
+      "pair": {
+        "reference": "..\\bitmaps_reference\\1222062857_test-card-hover_0_data-qacard_0_tablet_h.png",
+        "test": "..\\bitmaps_test\\20190728-111558\\1222062857_test-card-hover_0_data-qacard_0_tablet_h.png",
+        "selector": "[data-qa=\"card\"]",
+        "fileName": "1222062857_test-card-hover_0_data-qacard_0_tablet_h.png",
+        "label": "test-card-hover",
+        "requireSameDimensions": false,
+        "misMatchThreshold": 1,
+        "url": "http://localhost:8080/index.html",
+        "referenceUrl": "https://mate-academy.github.io/layout_solutions/product-cards/",
+        "expect": 0,
+        "viewportLabel": "tablet_h",
+        "diff": {
+          "isSameDimensions": false,
+          "dimensionDifference": {
+            "width": 0,
+            "height": 1
+          },
+          "misMatchPercentage": "0.32",
+          "analysisTime": 13
+        }
+      },
+      "status": "pass"
+    }
+  ]
+});
