b4324d5de18be99c8bb5dac008d96c5f88008735
Sijey <popov040391@gmail.com>
2019-09-04 14:09:13 +0300

    deploy static content

---
+++ src/report/html_report/config.js
@@ -0,0 +1,55 @@
+report({
+  "testSuite": "BackstopJS",
+  "tests": [
+    {
+      "pair": {
+        "reference": "../bitmaps_reference/0108602439_test-card-default_0_data-qacard_0_tablet_h.png",
+        "test": "../bitmaps_test/20190904-140710/0108602439_test-card-default_0_data-qacard_0_tablet_h.png",
+        "selector": "[data-qa=\"card\"]",
+        "fileName": "0108602439_test-card-default_0_data-qacard_0_tablet_h.png",
+        "label": "test-card-default",
+        "requireSameDimensions": false,
+        "misMatchThreshold": 1,
+        "url": "http://localhost:8080/index.html",
+        "referenceUrl": "https://mate-academy.github.io/layout_solutions/product-cards/",
+        "expect": 0,
+        "viewportLabel": "tablet_h",
+        "diff": {
+          "isSameDimensions": true,
+          "dimensionDifference": {
+            "width": 0,
+            "height": 0
+          },
+          "misMatchPercentage": "0.92",
+          "analysisTime": 32
+        }
+      },
+      "status": "pass"
+    },
+    {
+      "pair": {
+        "reference": "../bitmaps_reference/0108602439_test-card-hover_0_data-qacard_0_tablet_h.png",
+        "test": "../bitmaps_test/20190904-140710/0108602439_test-card-hover_0_data-qacard_0_tablet_h.png",
+        "selector": "[data-qa=\"card\"]",
+        "fileName": "0108602439_test-card-hover_0_data-qacard_0_tablet_h.png",
+        "label": "test-card-hover",
+        "requireSameDimensions": false,
+        "misMatchThreshold": 1,
+        "url": "http://localhost:8080/index.html",
+        "referenceUrl": "https://mate-academy.github.io/layout_solutions/product-cards/",
+        "expect": 0,
+        "viewportLabel": "tablet_h",
+        "diff": {
+          "isSameDimensions": true,
+          "dimensionDifference": {
+            "width": 0,
+            "height": 0
+          },
+          "misMatchPercentage": "0.91",
+          "analysisTime": 43
+        }
+      },
+      "status": "pass"
+    }
+  ]
+});
