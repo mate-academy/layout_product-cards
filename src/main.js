7fcbdca6fb994c7581273a8e2867f8e29bccfaf2
bandura <banduraweb@ukr.net>
2019-10-22 16:07:11 +0300

    'solution'

--- src/main.js
+++ src/main.js
@@ -1,1 +1,9 @@
 'use strict';
+// document.addEventListener('DOMContentLoaded', onReady);
+// function onReady() {
+//   const btnBuy = document.querySelector('.ProductsBuy_button');
+//   btnBuy.onclick = function () {
+//     this.classList.toggle('ProductsBuy_button--hover');
+//     this.innerText='Buying...'
+//   }
+// }
