decc2e9ecd8875c8d8bd3b7c29613cadcd748771
Oleksii <alex4rest1@gmail.com>
2021-12-30 11:56:07 +0200

    fixed product-card

--- backstopConfig.js
+++ backstopConfig.js
@@ -7,7 +7,7 @@ const { basicScenario } = backstop;
 const basic = {
   ...basicScenario,
   label: 'Elementary test',
-  referenceUrl: basicScenario.referenceUrl + '/cards/',
+  referenceUrl: basicScenario.referenceUrl + '/product-cards/',
 };
 
 const config = {
