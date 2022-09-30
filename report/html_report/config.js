bed254d4b5a87630a2ab30b10448aa9f4ecf08a9
drhtka <drhtka@gmil.com>
2022-09-30 09:23:12 +0300

    fix 30092022

--- report/html_report/config.js
+++ report/html_report/config.js
@@ -4,7 +4,7 @@ report({
     {
       "pair": {
         "reference": "..\\bitmaps_reference\\Card_with_data-qa_card.png",
-        "test": "..\\bitmaps_test\\20220924-215130\\Card_with_data-qa_card.png",
+        "test": "..\\bitmaps_test\\20220930-091803\\Card_with_data-qa_card.png",
         "selector": "[data-qa=\"card\"]",
         "fileName": "Card_with_data-qa_card.png",
         "label": "Card with data-qa_card",
@@ -20,8 +20,8 @@ report({
             "width": 0,
             "height": 0
           },
-          "misMatchPercentage": "0.71",
-          "analysisTime": 32
+          "misMatchPercentage": "0.44",
+          "analysisTime": 25
         }
       },
       "status": "pass"
@@ -29,7 +29,7 @@ report({
     {
       "pair": {
         "reference": "..\\bitmaps_reference\\Link_with_data-qa_hover.png",
-        "test": "..\\bitmaps_test\\20220924-215130\\Link_with_data-qa_hover.png",
+        "test": "..\\bitmaps_test\\20220930-091803\\Link_with_data-qa_hover.png",
         "selector": "[data-qa=\"card\"]",
         "fileName": "Link_with_data-qa_hover.png",
         "label": "Link with data-qa_hover",
@@ -45,8 +45,8 @@ report({
             "width": 0,
             "height": 0
           },
-          "misMatchPercentage": "0.71",
-          "analysisTime": 29
+          "misMatchPercentage": "0.44",
+          "analysisTime": 24
         }
       },
       "status": "pass"
