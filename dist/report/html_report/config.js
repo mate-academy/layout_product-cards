dfe80d4d1468bad9f07ed4da5403ad51fc7df356
Volodymyr Borysov <qweqweqw1112fdg@gmail.com>
2023-01-27 21:28:51 +0200

    make build

--- dist/report/html_report/config.js
+++ dist/report/html_report/config.js
@@ -4,7 +4,7 @@ report({
     {
       "pair": {
         "reference": "..\\bitmaps_reference\\Card_with_data-qa_card.png",
-        "test": "..\\bitmaps_test\\20230127-203839\\Card_with_data-qa_card.png",
+        "test": "..\\bitmaps_test\\20230127-212605\\Card_with_data-qa_card.png",
         "selector": "[data-qa=\"card\"]",
         "fileName": "Card_with_data-qa_card.png",
         "label": "Card with data-qa_card",
@@ -15,13 +15,13 @@ report({
         "expect": 0,
         "viewportLabel": "tablet_h",
         "diff": {
-          "isSameDimensions": false,
+          "isSameDimensions": true,
           "dimensionDifference": {
             "width": 0,
-            "height": -2
+            "height": 0
           },
           "misMatchPercentage": "0.17",
-          "analysisTime": 28
+          "analysisTime": 19
         }
       },
       "status": "pass"
@@ -29,7 +29,7 @@ report({
     {
       "pair": {
         "reference": "..\\bitmaps_reference\\Link_with_data-qa_hover.png",
-        "test": "..\\bitmaps_test\\20230127-203839\\Link_with_data-qa_hover.png",
+        "test": "..\\bitmaps_test\\20230127-212605\\Link_with_data-qa_hover.png",
         "selector": "[data-qa=\"card\"]",
         "fileName": "Link_with_data-qa_hover.png",
         "label": "Link with data-qa_hover",
@@ -43,10 +43,10 @@ report({
           "isSameDimensions": false,
           "dimensionDifference": {
             "width": 0,
-            "height": 2
+            "height": -2
           },
           "misMatchPercentage": "0.74",
-          "analysisTime": 26
+          "analysisTime": 24
         }
       },
       "status": "pass"
