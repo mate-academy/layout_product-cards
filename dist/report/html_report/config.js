2f12a75718899ac292ea3701b4226f1c2b74e167
Robert Mans <robert.mans.dev@gmail.com>
2025-04-01 20:50:44 +0100

    make build

---
+++ dist/report/html_report/config.js
@@ -0,0 +1,58 @@
+report({
+  "testSuite": "BackstopJS",
+  "tests": [
+    {
+      "pair": {
+        "reference": "..\\bitmaps_reference\\Card_with_data-qa_card.png",
+        "test": "..\\bitmaps_test\\20250401-204755\\Card_with_data-qa_card.png",
+        "selector": "[data-qa=\"card\"]",
+        "fileName": "Card_with_data-qa_card.png",
+        "label": "Card with data-qa_card",
+        "requireSameDimensions": false,
+        "misMatchThreshold": 1,
+        "url": "http://localhost:3001/index.html",
+        "referenceUrl": "https://mate-academy.github.io/layout_solutions/product-cards/",
+        "expect": 0,
+        "viewportLabel": "tablet_h",
+        "diff": {
+          "isSameDimensions": true,
+          "dimensionDifference": {
+            "width": 0,
+            "height": 0
+          },
+          "rawMisMatchPercentage": 0.8137254901960784,
+          "misMatchPercentage": "0.81",
+          "analysisTime": 19
+        }
+      },
+      "status": "pass"
+    },
+    {
+      "pair": {
+        "reference": "..\\bitmaps_reference\\Link_with_data-qa_hover.png",
+        "test": "..\\bitmaps_test\\20250401-204755\\Link_with_data-qa_hover.png",
+        "selector": "[data-qa=\"card\"]",
+        "fileName": "Link_with_data-qa_hover.png",
+        "label": "Link with data-qa_hover",
+        "requireSameDimensions": false,
+        "misMatchThreshold": 1,
+        "url": "http://localhost:3001/index.html",
+        "referenceUrl": "https://mate-academy.github.io/layout_solutions/product-cards/",
+        "expect": 0,
+        "viewportLabel": "tablet_h",
+        "diff": {
+          "isSameDimensions": false,
+          "dimensionDifference": {
+            "width": 0,
+            "height": 1
+          },
+          "rawMisMatchPercentage": 1.1102941176470589,
+          "misMatchPercentage": "1.11",
+          "analysisTime": 18
+        },
+        "diffImage": "..\\bitmaps_test\\20250401-204755\\failed_diff_Link_with_data-qa_hover.png"
+      },
+      "status": "fail"
+    }
+  ]
+});
