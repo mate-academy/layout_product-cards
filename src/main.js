c92b1539c7e718f58431d228447a434d40c871bc
A-Melnykk <969696245n@gmail.com>
2025-04-15 16:22:28 +0300

    123

--- src/main.js
+++ src/main.js
@@ -1,1 +1,11 @@
-'use strict';
+const stars = document.querySelectorAll('#rating-stars .stars__star');
+const starsWrapper = document.getElementById('rating-stars');
+
+stars.forEach(star => {
+  star.addEventListener('click', () => {
+    const rating = star.getAttribute('data-value');
+    starsWrapper.className = `stars stars--${rating}`;
+  });
+});
+
+
