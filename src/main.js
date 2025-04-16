0da20ebef668156208596fa236c47f46a1d3a343
Peterson <poseidoncenghizerzhemetmhossad@outlook.com>
2025-04-16 03:13:06 -0300

    Update main.js

--- src/main.js
+++ src/main.js
@@ -1,1 +1,16 @@
 'use strict';
+
+document.addEventListener('DOMContentLoaded', () => {
+  const starsBlocks = document.querySelectorAll('.stars');
+
+  starsBlocks.forEach((stars) => {
+    stars.addEventListener('click', (e) => {
+      if (e.target.classList.contains('stars__star')) {
+        const index = [...stars.children].indexOf(e.target) + 1;
+        stars.className = `stars stars--${index}`;
+      }
+    });
+  });
+});
+
+
