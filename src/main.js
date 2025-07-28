217521d8174f838c1c10a2e196bf65889e0d3397
Demetrio dos Anjos <ddoanjos@gmail.com>
2025-07-28 01:41:43 -0300

    sua mensagem aqui

--- src/main.js
+++ src/main.js
@@ -1,1 +1,8 @@
-'use strict';
+
+fetch('./stars.html')
+  .then(response => response.text())
+  .then(html => {
+    document.querySelectorAll('.card__stars').forEach(container => {
+      container.innerHTML = html;
+    });
+  });
