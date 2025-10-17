a0cb269eefbf96c0ef46b8dc7d09e75e39cfe1bd
mezavg <tosha.linberg2@gmail.com>
2025-10-17 19:53:05 +0300

    m1

--- backstop_data/engine_scripts/puppet/clickAndHoverHelper.js
+++ backstop_data/engine_scripts/puppet/clickAndHoverHelper.js
@@ -1,9 +1,9 @@
 module.exports = async (page, scenario) => {
-  var hoverSelector = scenario.hoverSelectors || scenario.hoverSelector;
-  var clickSelector = scenario.clickSelectors || scenario.clickSelector;
-  var keyPressSelector = scenario.keyPressSelectors || scenario.keyPressSelector;
-  var scrollToSelector = scenario.scrollToSelector;
-  var postInteractionWait = scenario.postInteractionWait; // selector [str] | ms [int]
+  const hoverSelector = scenario.hoverSelectors || scenario.hoverSelector;
+  const clickSelector = scenario.clickSelectors || scenario.clickSelector;
+  const keyPressSelector = scenario.keyPressSelectors || scenario.keyPressSelector;
+  const scrollToSelector = scenario.scrollToSelector;
+  const postInteractionWait = scenario.postInteractionWait; // selector [str] | ms [int]
 
   if (keyPressSelector) {
     for (const keyPressSelectorItem of [].concat(keyPressSelector)) {
@@ -27,7 +27,9 @@ module.exports = async (page, scenario) => {
   }
 
   if (postInteractionWait) {
-    await new Promise(resolve => setTimeout(resolve, postInteractionWait));
+    await new Promise(resolve => {
+      setTimeout(resolve, postInteractionWait);
+    });
   }
 
   if (scrollToSelector) {
