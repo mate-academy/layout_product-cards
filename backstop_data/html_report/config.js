582ce1f9effb8588bee1c26a4b1f8af7c11432ee
Yurii Buriak <yuriiburiak8@gmail.com>
2025-10-17 17:07:24 +0200

    Add test report for GitHub Pages

---
+++ backstop_data/html_report/config.js
@@ -0,0 +1,59 @@
+report({
+  "testSuite": "BackstopJS",
+  "tests": [
+    {
+      "pair": {
+        "reference": "..\\bitmaps_reference\\Card_with_data-qa_card.png",
+        "test": "..\\bitmaps_test\\20251017-170153\\Card_with_data-qa_card.png",
+        "selector": "[data-qa=\"card\"]",
+        "fileName": "Card_with_data-qa_card.png",
+        "label": "Card with data-qa_card",
+        "requireSameDimensions": false,
+        "misMatchThreshold": 1,
+        "url": "http://localhost:3001/index.html",
+        "referenceUrl": "https://mate-academy.github.io/layout_solutions/product-cards/",
+        "expect": 0,
+        "viewportLabel": "tablet_h",
+        "diff": {
+          "isSameDimensions": true,
+          "dimensionDifference": {
+            "width": 0,
+            "height": 0
+          },
+          "rawMisMatchPercentage": 47.22058823529412,
+          "misMatchPercentage": "47.22",
+          "analysisTime": 10
+        },
+        "diffImage": "..\\bitmaps_test\\20251017-170153\\failed_diff_Card_with_data-qa_card.png"
+      },
+      "status": "fail"
+    },
+    {
+      "pair": {
+        "reference": "..\\bitmaps_reference\\Link_with_data-qa_hover.png",
+        "test": "..\\bitmaps_test\\20251017-170153\\Link_with_data-qa_hover.png",
+        "selector": "[data-qa=\"card\"]",
+        "fileName": "Link_with_data-qa_hover.png",
+        "label": "Link with data-qa_hover",
+        "requireSameDimensions": false,
+        "misMatchThreshold": 1,
+        "url": "http://localhost:3001/index.html",
+        "referenceUrl": "https://mate-academy.github.io/layout_solutions/product-cards/",
+        "expect": 0,
+        "viewportLabel": "tablet_h",
+        "diff": {
+          "isSameDimensions": true,
+          "dimensionDifference": {
+            "width": 0,
+            "height": 0
+          },
+          "rawMisMatchPercentage": 34.14705882352941,
+          "misMatchPercentage": "34.15",
+          "analysisTime": 10
+        },
+        "diffImage": "..\\bitmaps_test\\20251017-170153\\failed_diff_Link_with_data-qa_hover.png"
+      },
+      "status": "fail"
+    }
+  ]
+});
